b2b8a07b1d0f0045f913acbf08c3c9ac81e143de
LiHuaJun <1664380668@qq.com>
2020-11-10 23:24:54 +0800

    路由初始化

---
+++ blog-1/src/router/user.js
@@ -0,0 +1,15 @@
+const handleUserRouter = (req, res) => {
+  const method = req.method
+  const url = req.url
+  const path = url.split('?')[0]
+
+  
+  // 用户登录
+  if (method === 'POST' && path === '/api/user/login') {
+    return {
+      msg: '这是登录的接口'
+    }
+  }
+}
+
+module.exports = handleUserRouter
