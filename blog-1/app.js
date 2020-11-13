2e8030655136d65ed1bd071c13cf468be72a10d9
LiHuaJun <1664380668@qq.com>
2020-11-13 22:11:10 +0800

    路由-新建，更新，删除

--- blog-1/app.js
+++ blog-1/app.js
@@ -2,39 +2,80 @@ const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+// 用于处理 post data ，若是POST请求且存在data ,则返回JSON.parse(postData)
+const getPostData = (req) => {
+  return new Promise((resolve,reject) => {
+    if(req.method !== 'POST') {
+      resolve({})
+      return
+    }
+    if(req.headers['content-type' !== 'application/json']) {
+      resolve({})
+      return
+    }
+    // 获取客户端传过来的数据
+    let postData = ''
+    req.on('data',chunk => {
+      postData += chunk.toString()
+    })
+    req.on('end', () => {
+      if (!postData) {
+        resolve({})
+        return
+      }
+      resolve(
+        JSON.parse(postData)
+      )
+    })
+  })
+}
 
 const serverHandle = (req,res) => {
   //设置返回格式
   res.setHeader('Content-type','application/json')
+
   // 获取path
   const url = req.url
   req.path = url.split('?')[0]
 
   // 解析query
-  req.query = querystring.parse(url.split('?')[0])
-
-  // 处理blog路由
-  const blogData = handleBlogRouter(req,res)
-  if(blogData) {
-    res.end(
-      JSON.stringify(blogData)
-    )
-    return
-  }
+  req.query = querystring.parse(url.split('?')[1])
   
-  // 处理user路由
-  const userData = handleUserRouter(req,res)
-  if(userData) {
-    res.end(
-      JSON.stringify(userData)
-    )
-    return
-  }
 
-  // 未命中路由，返回404
-  res.writeHead(404, {"Content-type": "text/plain"})
-  res.write("404 Not Found\n")
-  res.end()
+  // 在进入路由之前，先处理post data
+  getPostData(req).then(postData => {
+    // 所有路由可以通过req.body来获取postData
+    req.body = postData
+
+    /**
+     * 获取到postData后再进入路由
+     */
+    // 处理blog路由
+    const blogData = handleBlogRouter(req,res)
+    if(blogData) {
+      res.end(
+        JSON.stringify(blogData)
+      )
+      return
+    }
+    
+    // 处理user路由
+    const userData = handleUserRouter(req,res)
+    if(userData) {
+      res.end(
+        JSON.stringify(userData)
+      )
+      return
+    }
+
+    // 未命中路由，返回404
+    res.writeHead(404, {"Content-type": "text/plain"})
+    res.write("404 Not Found\n")
+    res.end()
+    /** 
+     * 获取到postData后再进入路由
+     */
+  })
 }
 
 module.exports = serverHandle
--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -18,6 +18,47 @@ const getList = (author,keyword) => {
   ]
 }
 
+const getDetail = (id) => {
+  // 先返回假数据
+  return {
+    id: 1,
+    title: '标题1',
+    content: '内容A',
+    createTime: 1605080082985,
+    author: 'zhangsan'
+  }
+}
+
+const newBlog = (blogData = {}) => {
+  // blogData是一个博客对象，包含title content 属性
+  // console.log('newBlog blogData',blogData);
+  
+  return {
+    id: 3 //表示新建博客， 插入到数据表里面的id
+  }
+}
+
+const updateBlog = (id, blogData = {}) => {
+  // blogData是一个博客对象，包含title content 属性
+  // console.log('updateBlog id,blogData',id,blogData);
+
+  return true
+  //返回true表示更新成功
+}
+
+const delBlog = (id) => {
+  // 播客id
+  console.log(id);
+  
+
+  return true
+  //返回true表示更新成功
+}
+
 module.exports = {
-  getList
+  getList,
+  getDetail,
+  newBlog, 
+  updateBlog,
+  delBlog
 }
